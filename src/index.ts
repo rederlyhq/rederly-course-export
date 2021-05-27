2384ae659adce5dd11e71df8905b644408becbee
red-tt-bear <red.tt.bear@gmail.com>
2021-05-27 16:34:18 -0400

    Broke script into more concise methods and added comments
    Fixed some bugs too:
    1. Make it so the folder at the root of the tarball is the name of the
    folder rather than `.`
    2. Added a `units` folder (this will avoid conflicts in case someone
    named their topic private)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { Op, Sequelize } from "sequelize";
 import Course from "./database/models/course";
 import CourseTopicContent from "./database/models/course-topic-content";
 import CourseUnitContent from "./database/models/course-unit-content";
@@ -13,6 +12,9 @@ import CourseQuestionAssessmentInfo from "./database/models/course-question-asse
 import configurations from "./configurations";
 import tar from 'tar';
 
+const { workingTempDirectory, webworkFileLocation } = configurations.paths;
+
+/** TODO move below to shared space because this is shared with the backend */
 const httpNegativeLookAhead = '(?!\\s*https?:)';
 const assetInPgFileExtensions = '(?:' + // Non capture group to or all the extensions together
 [
@@ -53,48 +55,98 @@ export const imageInPGFileRegex = new RegExp(
         ')', // end non capture group for negative look behind
     ].join(''), 'g'
 );
+/** TODO move above to shared space because this is shared with the backend */
 
-const getPrivateProblemsInCourse = async (courseId: number) => {
-    const privateQuestions: CourseWWTopicQuestion[] = await CourseWWTopicQuestion.findAll({
-        include: [{
-            model: CourseTopicContent,
-            as: 'topic',
-            where: {
-                active: true,
-            },
-            include: [{
-                model: CourseUnitContent,
-                as: 'unit',
-                where: {
-                    active: true,
-                    courseId: courseId,
-                }
-                // include: [{
-                //     model: Course,
-                //     as: 'course',
-                //     where: {
-                //         instructorId: userId
-                //     }
-                // }]
-            }]
-        }],
-        where: {
-            webworkQuestionPath: {
-                [Op.like]: 'private/%'
-            },
-            active: true
-        },
-        attributes: [
-            'webworkQuestionPath'
-        ]
-    });
-    const privateProblemPaths = [...new Set(privateQuestions.map(question => question.webworkQuestionPath))];
-    return privateProblemPaths;
+/**
+ * 
+ * @param course The course to flatten and get filter private problem paths from
+ * @returns An array of problem paths that need to be copied
+ */
+const getPrivateProblemPathsFromCourse = (course: Course): string[] => {
+    return _.uniq(_.flattenDeep(
+        course.units?.map(
+            unit => unit.topics?.map(
+                topic => topic.questions?.map(
+                    question => question.webworkQuestionPath
+                ) ?? [] as string[]
+            ) ?? [] as string[]
+        ) ?? [] as string[]
+    ).filter((wwPath: string) => wwPath.startsWith('private/')))
 };
 
-export const generateDefFiles = async (courseId: number, tmpDirectory: string) => {
-    const workingDirectory = path.join(tmpDirectory, courseId.toString());
-    const course: Course = await Course.findOne({
+/**
+ * For development, this code generates files to make sure the packaging works as expected
+ */
+const generateTestPrivateFiles = async (privateProblemPaths: string[]) => {
+    const testPromises = privateProblemPaths.map(async (privateProblemPath) => {
+        const filepath = `${webworkFileLocation}/${privateProblemPath}`;
+        console.log(`Generating: ${filepath}`);
+        await fs.mkdirp(path.dirname(filepath));
+        if (fs.existsSync(filepath)) {
+            console.warn('Dev code to generate private files run but file exists');
+        } else {
+            await fs.writeFile(filepath, `STARTTEST ${privateProblemPath} TESTEND`);
+        }
+    });
+    await Promise.all(testPromises);
+}
+
+/**
+ * Copy private files to temp folder for tarring
+ * @param course the course to copy private files for
+ * @param privateProblemPaths the paths of the private problems
+ * @param contentDirectory where the private files should go
+ */
+const copyPrivateFiles = async (course: Course, privateProblemPaths: string[], contentDirectory: string) => {
+    const assetPromises: Promise<void>[] = [];
+    const privateCopyPromises = privateProblemPaths.map(async (privateProblemPath) => {
+        const from = path.join(webworkFileLocation, privateProblemPath);
+        const to = path.join(workingTempDirectory, course.id.toString(), course.name, privateProblemPath);
+        await fs.mkdirp(path.dirname(to));
+        await fs.copy(from, to);
+        const pgContent = (await fs.readFile(from)).toString();
+        for(const match of pgContent.matchAll(imageInPGFileRegex)) {
+            let imagePath: string = match[1] ?? match[2];
+
+            perlQuotes.some(quote => {
+                const insideRegex = new RegExp(`${quote[0]}(.*)${quote[1]}`, 'g');
+                const matches = imagePath.matchAll(insideRegex);
+                // if (matches.length > 1) {
+                //     console.warn(`findFilesFromPGFile: insideRegex expected 1 match but got ${matches.length}`);
+                // }
+                // Will not be nil if different quotes
+                const thisMatch = matches.next().value;
+                if (!_.isNil(thisMatch)) {
+                    // index 1 should be first capture group, should not be nil
+                    if (_.isNil(thisMatch[1])) {
+                        console.error(`findFilesFromPGFile: No capture group for quote ${quote[0]}`);
+                    } else {
+                        imagePath = thisMatch[1];
+                    }
+                    // bow out
+                    return true;
+                }
+                return false;
+            });
+            const imageFrom = path.join(webworkFileLocation, path.dirname(privateProblemPath), imagePath);
+            const imageTo = path.join(workingTempDirectory, course.id.toString(), course.name, path.dirname(privateProblemPath), imagePath);
+            assetPromises.push(fs.copy(imageFrom, imageTo).catch(e => { throw new Error(`Prob: ${to}; ${e.toString()}`)}))
+        }
+    });
+    const privateCopyErrorPromises = (await Promise.allSettled(privateCopyPromises)).map(async (promise) => promise.status === 'rejected' && fs.appendFile(path.join(contentDirectory, 'privateFileErrors.txt'), `${promise.reason.toString()}\n`));
+    const assetCopyErrorPromises = (await Promise.allSettled(assetPromises)).map(async (promise) => promise.status === 'rejected' &&
+    fs.appendFile(path.join(contentDirectory, 'privateFileErrors.txt'), `${promise.reason.toString()}\n`));
+    await Promise.all(privateCopyErrorPromises);
+    await Promise.all(assetCopyErrorPromises);
+}
+
+/**
+ * Get data from database for export
+ * @param courseId The id of the course to fetch
+ * @returns A course with it's units, topics, exam topic info, questions, exam question info
+ */
+const fetchData = async (courseId: number): Promise<Course> => {
+    const course: Course | null = await Course.findOne({
         where: {
             active: true,
             id: courseId
@@ -146,9 +198,30 @@ export const generateDefFiles = async (courseId: number, tmpDirectory: string) =
         }]
     });
 
+    if (_.isNil(course)) {
+        throw new Error(`Course (${courseId}) not found.`)
+    }
+    return course;
+}
+
+/**
+ * Iterate through all the topics and create def files for each one
+ * File structure:
+ * /{COURSE_NAME}/privateFileErrors.txt // error that came up during export
+ * /{COURSE_NAME}/{RENDERER_PATHS} // RENDERER_PATHS will mostly be private/my/{UUID}/{REMAINING_PATH}.(pg|png|gif|etc.)
+ * /{COURSE_NAME}/units/{UNIT_NAME}/{TOPIC_NAME}.def // Each unit is a folder and contains a topic file for each of it's topics
+ * @param course The course (with units, topics, exam topic info, questions, exam question info)
+ * @param tmpDirectory The location that temp files are store
+ * @returns {
+ * contentDirectory: The directory that will eventually be tarred up
+ * }
+ */
+const generateDefFiles = async (course: Course, tmpDirectory: string) => {
+    const workingDirectory = path.join(tmpDirectory, course.id.toString());
+
     const contentDirectory = path.join(workingDirectory, course.name);
     const unitPromises = course.units?.map(async (unit) => {
-        const unitPath = path.join(contentDirectory, unit.name);
+        const unitPath = path.join(contentDirectory, 'units', unit.name);
         await fs.mkdirp(unitPath);
         const topicPromises = unit.topics?.map(async (topic) => {
             if (_.isNil(topic.questions)) {
@@ -162,93 +235,47 @@ export const generateDefFiles = async (courseId: number, tmpDirectory: string) =
     });
     await Promise.all(unitPromises ?? [Promise.resolve()]);
     return {
-        course: course,
-        workingDirectory: workingDirectory,
         contentDirectory: contentDirectory
     }
 }
+
+/**
+ * In the parent directory of the content directory (which should be the working directory) tar up the content directory
+ * @param contentDirectory The directory to tar up
+ * @returns a promise that resolves when the stream finishes or rejects on error
+ */
+const tarUp = (contentDirectory: string) => new Promise((resolve, reject) => {
+    const name = path.basename(contentDirectory);
+    const workingDirectory = path.dirname(contentDirectory);
+    const tarStream = tar.create({
+        gzip: true,
+        cwd: path.resolve(workingDirectory)
+    }, [name]
+    ).pipe(fs.createWriteStream(path.join(workingDirectory, `${name}.tgz`)))
+
+    tarStream.on('finish', resolve);
+    tarStream.on('error', reject);
+});
+
 (async () => {
-    const { workingTempDirectory, webworkFileLocation } = configurations.paths;
     const courseId = -1;
     if(fs.existsSync(workingTempDirectory)) {
         await fs.remove(workingTempDirectory);
     }
-    await fs.promises.mkdir(workingTempDirectory);
+    await fs.mkdirp(workingTempDirectory);
+
+    const course = await fetchData(courseId);
     const {
-        course,
-        workingDirectory,
         contentDirectory
-    } = await generateDefFiles(courseId, workingTempDirectory);
+    } = await generateDefFiles(course, workingTempDirectory);
 
     // const privateProblemPaths = await getPrivateProblemsInCourse(courseId);
-    const privateProblemPaths = _.uniq(_.flattenDeep(
-        course.units?.map(
-            unit => unit.topics?.map(
-                topic => topic.questions?.map(
-                    question => question.webworkQuestionPath
-                ) ?? [] as string[]
-            ) ?? [] as string[]
-        ) ?? [] as string[]
-    ).filter((wwPath: string) => wwPath.startsWith('private/')));
+    const privateProblemPaths = getPrivateProblemPathsFromCourse(course);
+    // generateTestPrivateFiles(privateProblemPaths);
 
-    // const testPromises = privateProblemPaths.map(async (privateProblemPath) => {
-    //     const filepath = `${webworkFileLocation}/${privateProblemPath}`;
-    //     console.log(path.dirname(filepath));
-    //     await fs.mkdirp(path.dirname(filepath));
-    //     await fs.writeFile(filepath, `STARTTOM ${privateProblemPath} TOMEND`);
-    // });
-    // await Promise.all(testPromises);
+    await copyPrivateFiles(course, privateProblemPaths, contentDirectory);
 
-    const assetPromises: Promise<void>[] = [];
-    const privateCopyPromises = privateProblemPaths.map(async (privateProblemPath) => {
-        const from = path.join(webworkFileLocation, privateProblemPath);
-        const to = path.join(workingTempDirectory, course.id.toString(), course.name, privateProblemPath);
-        await fs.mkdirp(path.dirname(to));
-        await fs.copy(from, to);
-        const pgContent = (await fs.readFile(from)).toString();
-        for(const match of pgContent.matchAll(imageInPGFileRegex)) {
-            let imagePath: string = match[1] ?? match[2];
+    await tarUp(contentDirectory);
 
-            perlQuotes.some(quote => {
-                const insideRegex = new RegExp(`${quote[0]}(.*)${quote[1]}`, 'g');
-                const matches = imagePath.matchAll(insideRegex);
-                // if (matches.length > 1) {
-                //     console.warn(`findFilesFromPGFile: insideRegex expected 1 match but got ${matches.length}`);
-                // }
-                // Will not be nil if different quotes
-                const thisMatch = matches.next().value;
-                if (!_.isNil(thisMatch)) {
-                    // index 1 should be first capture group, should not be nil
-                    if (_.isNil(thisMatch[1])) {
-                        console.error(`findFilesFromPGFile: No capture group for quote ${quote[0]}`);
-                    } else {
-                        imagePath = thisMatch[1];
-                    }
-                    // bow out
-                    return true;
-                }
-                return false;
-            });
-            const imageFrom = path.join(webworkFileLocation, path.dirname(privateProblemPath), imagePath);
-            const imageTo = path.join(workingTempDirectory, course.id.toString(), course.name, path.dirname(privateProblemPath), imagePath);
-            assetPromises.push(fs.copy(imageFrom, imageTo).catch(e => { throw new Error(`Prob: ${to}; ${e.toString()}`)}))
-        }
-    });
-    const privateCopyErrorPromises = (await Promise.allSettled(privateCopyPromises)).map(async (promise) => promise.status === 'rejected' && fs.appendFile(path.join(contentDirectory, 'privateFileErrors.txt'), `${promise.reason.toString()}\n`));
-    const assetCopyErrorPromises = (await Promise.allSettled(assetPromises)).map(async (promise) => promise.status === 'rejected' &&
-    fs.appendFile(path.join(contentDirectory, 'privateFileErrors.txt'), `${promise.reason.toString()}\n`));
-    await Promise.all(privateCopyErrorPromises);
-    await Promise.all(assetCopyErrorPromises);
-
-    const tarStream = tar.create({
-        gzip: true,
-        cwd: path.resolve(contentDirectory)
-    }, ['.']
-    ).pipe(fs.createWriteStream(path.join(workingDirectory, `${course.name}.tgz`)))
-
-    await new Promise((resolve, reject) => {
-        tarStream.on('finish', resolve);
-        tarStream.on('error', reject);
-    })
     // await fs.remove(path.join(workingTempDirectory, courseId.toString()));
-})();
+})().catch(err => console.error(err));
