daa0375cdda1fbdc1aab3b9be6179c356524e864
red-tt-bear <red.tt.bear@gmail.com>
2021-05-30 00:05:26 -0400

    Added dotenv config location configuration
    This will allow us to run multiple servers from the same box easily

--- server/src/configurations.ts
+++ server/src/configurations.ts
@@ -1,5 +1,8 @@
+import path from 'path';
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({
+    path: process.env.DOTENV_LOCATION ? path.resolve(process.env.DOTENV_LOCATION) : undefined
+});
 import * as _ from 'lodash';
 import * as crypto from 'crypto';
 
