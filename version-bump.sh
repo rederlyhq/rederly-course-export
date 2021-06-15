41910e8a5fc9fcf0b4799c065ae3030257dc4fef
red-tt-bear <red.tt.bear@gmail.com>
2021-06-15 10:50:44 -0400

    Added auto commit

--- version-bump.sh
+++ version-bump.sh
@@ -4,9 +4,14 @@ set -e
 
 VERSION_ARG=$1
 [ "$VERSION_ARG" = "" ] && echo "Missing version arg" && exit 1
-VERSION_NUMBER="$(npm version "$VERSION_ARG")"
 PROJECT_NAME="$(basename "$(pwd)")"
 
+[ "$(git diff --stat)" != '' ] && echo 'Cannot version bump when working copy is dirty' && exit 1
+VERSION_NUMBER="$(npm version "$VERSION_ARG")"
 TAG_STRING="$PROJECT_NAME--$VERSION_NUMBER"
+
+git add .
+git commit -m "Version bump: $TAG_STRING"
+
 git tag "$TAG_STRING" -m "$TAG_STRING"
 echo "success $TAG_STRING"
