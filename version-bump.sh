3fbd160e45a973599ed83b862ac2d6f94e53b4fb
red-tt-bear <red.tt.bear@gmail.com>
2021-06-15 10:44:33 -0400

    Added output of tag string and message

--- version-bump.sh
+++ version-bump.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-VERSION_NUMBER="$(npm version "$1")"
+
+set -e
+
+VERSION_ARG=$1
+[ "$VERSION_ARG" = "" ] && echo "Missing version arg" && exit 1
+VERSION_NUMBER="$(npm version "$VERSION_ARG")"
 PROJECT_NAME="$(basename "$(pwd)")"
 
-git tag "$PROJECT_NAME--$VERSION_NUMBER"
+TAG_STRING="$PROJECT_NAME--$VERSION_NUMBER"
+git tag "$TAG_STRING" -m "$TAG_STRING"
+echo "success $TAG_STRING"
